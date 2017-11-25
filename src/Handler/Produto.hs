59a26641c0c6851c9c71ddbe8decd808510d5128
Haskell <algarcia@usp.br>
2017-11-25 17:13:14 +0000

    17.11.25.6 - INGRID BIANCA (8) - Criação da tabela AlergenoAluno

---
+++ src/Handler/Produto.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Produto where
+
+import Import
+
+import Database.Persist.Postgresql
+
+
+postProdutoInsereR :: Handler Value
+postProdutoInsereR = do
+    produto <- requireJsonBody :: Handler Produto
+    pid <- runDB $ insert produto
+    sendStatusJSON created201 (object ["data" .= (fromSqlKey pid)])
+
+
